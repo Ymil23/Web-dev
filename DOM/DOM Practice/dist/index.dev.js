339d3dc7a8abfbc018586762aa2b190c9bf094ea
Kamil <inaabdirahman24@gmail.com>
2026-02-16 18:04:07 +0530

    The Dice Game 1st Complition

---
+++ DOM/DOM Practice/dist/index.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var liElement = document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Kamil";
+document.querySelectorAll("button.btn")[0].style.backgroundColor = "blue"; //Siking everyone thinkinng that the link is for google but it is for a brilliant browser called Opera Mini :)
+
+document.querySelector(".item a").setAttribute("href", "https://www.opera.com/opera");
