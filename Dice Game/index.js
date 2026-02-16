3f4f863646d5ec97706e953468cee07fe3124bc4
Kamil <inaabdirahman24@gmail.com>
2026-02-16 21:04:45 +0530

    Modified

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,24 +1,31 @@
 
-var randomplayer1 = Math.floor(Math.random() * 6) + 1;//No1-6
+function rollDice(){
 
-var randomImage1 ="images/dice"+ randomplayer1 + ".png"; //images/dice1.png - images/dice6.png
+    var randomPlayer1 = Math.floor(Math.random() * 6) + 1;//No1-6
 
-var player1Image = document.querySelectorAll("img")[0];
-player1Image.setAttribute("src", randomImage1);
+    var randomImage1 ="images/dice"+ randomPlayer1 + ".png"; //images/dice1.png - images/dice6.png
+
+    var player1Image = document.querySelectorAll("img")[0];
+    player1Image.setAttribute("src", randomImage1);
 
 
-var randomPlayer2 = Math.floor(Math.random() * 6) + 1;
-var imageSourceP2 = "images/dice" + randomPlayer2 +".png";
+    var randomPlayer2 = Math.floor(Math.random() * 6) + 1;
+    var imageSourceP2 = "images/dice" + randomPlayer2 +".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", imageSourceP2);
+    document.querySelectorAll("img")[1].setAttribute("src", imageSourceP2);
 
-if (randomplayer1 > randomPlayer2) {
-    document.querySelector("h1").textContent = "Amaranthine Wins!";
+    if (randomPlayer1 > randomPlayer2) {
+            document.querySelector("h1").textContent = " Amaranthine Wins!";
+        }
+
+    else if (randomPlayer2 > randomPlayer1) {
+            document.querySelector("h1").innerHTML  = "Mandaweeye Wins!";
+        }
+
+    else {
+            document.querySelector("h1").innerHTML = "Its A Draw!";
+    }
+   
 }
-else if (randomPlayer1 < randomPlayer2) {
-    document.querySelector("h1").textContent ="Mandaweeye Wins!";
-}
 
-else {
-    document.querySelector("h1").innerHtml = "Its A Draw!";
-}
+rollDice();
