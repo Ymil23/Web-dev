126e8f1455d12439beaa10bc46d8331df0b284c7
Kamil <inaabdirahman24@gmail.com>
2026-02-19 22:58:29 +0530

    JQuery

---
+++ JQuery/index.js
@@ -0,0 +1,37 @@
+
+$("h1").click(function() {
+    $("h1").css("color","yellow");
+});
+
+
+    //Styling using JS VS JQuery;
+for(var i = 0; i < 5; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color = "blue";
+    })
+}
+
+    //JQuery Shortens this with just A few Lines of code;
+$("button").click(function() {
+    $("h1").css("color","blue");
+
+});
+
+    //Keydown Event Using JQuery;
+$("input").keydown(function(event){
+    console.log(event.key);
+});
+
+    //Using the Entire Document to listen to a click;
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+})
+
+    //Another Way of adding Event listener is by Using 'on' keyword;
+$("h1").on("mouseover", function(){
+    $("h1").html("Stop Touching Me");
+
+    setTimeout(function(){
+        $("h1").html("Kamil ina Abdirahman");
+    }, 900);
+})
