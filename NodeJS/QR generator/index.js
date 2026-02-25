941eba1403d8a2295fe7ee10159eff98f14fb154
Kamil <inaabdirahman24@gmail.com>
2026-02-25 23:59:47 +0530

    NodeJs

--- NodeJS/QR generator/index.js
+++ NodeJS/QR generator/index.js
@@ -1,4 +1,4 @@
-import inquirer from "inquirer";
+
 import qr from "qr-image";
 import fs from "fs";
 
@@ -7,14 +7,17 @@ inquirer
     message:"Whats Your URL",
     name:"URL"
   },
+  
   ])
   .then((answers) => {
     const url = answers.URL;
 
+    //Saving the url as an image;
     var qr_svg = qr.image(url,{type: 'png'});
     qr_svg.pipe(fs.createWriteStream('qRimage.png'));
     console.log("QR Generated");
   })
+
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
