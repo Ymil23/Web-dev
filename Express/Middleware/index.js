4df11b201601ff75c2b72c3bf201942569027422
Kamil <inaabdirahman24@gmail.com>
2026-02-26 06:43:31 +0530

    Express Middleware project

---
+++ Express/Middleware/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));//filePath
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));//passing as url
+
+app.post('/submit',(req, res) => {
+    console.log(req.body);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +"/index.html");
+    console.log("The Path is: "+__dirname +"/index.html");
+});
+
+
+app.listen(port,()  => {
+    console.log(`Server runnning on port ${port}`);
+});
